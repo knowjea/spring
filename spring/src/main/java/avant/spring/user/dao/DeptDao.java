7372390cb9dfeb1d8690662ce11ebcccd10668e5
gyeongjaegwon <knowjea69@gmail.com>
2021-12-14 23:00:08 +0900

    Dept 관련 클래스 생성

---
+++ spring/src/main/java/avant/spring/user/dao/DeptDao.java
@@ -0,0 +1,14 @@
+package avant.spring.user.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class DeptDao {
+	ConnectionMaker connectionMaker;
+
+	public DeptDao(ConnectionMaker connectionMaker) {
+		this.connectionMaker = connectionMaker;
+	}
+
+}
