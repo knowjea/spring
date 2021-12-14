7f5765b89fa15f6bd0fcb4a5492cade0ab2f3d78
gyeongjaegwon <knowjea69@gmail.com>
2021-12-14 21:54:19 +0900

    init

---
+++ spring/src/main/java/avant/spring/user/UserTestMain.java
@@ -0,0 +1,34 @@
+package avant.spring.user;
+
+import java.sql.SQLException;
+
+import avant.spring.user.dao.UserDao;
+import avant.spring.user.domain.User;
+
+/**
+ *
+ *
+ * @author NB-18113007
+ * @history
+ *          2021. 12. 3. initial creation
+ */
+public class UserTestMain {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao dao = new UserDao();
+
+		User user = new User();
+		user.setId("test");
+		user.setName("aaA");
+		user.setPassword("asdsad");
+
+		dao.add(user);
+
+		System.out.println(user.getId() + " 등록 성공");
+
+		User user2 = dao.get(user.getId());
+
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+
+}
---
+++ spring/src/main/java/avant/spring/user/domain/User.java
@@ -0,0 +1,58 @@
+package avant.spring.user.domain;
+
+/**
+ *
+ *
+ * @author NB-18113007
+ * @history
+ *          2021. 12. 3. initial creation
+ */
+public class User {
+
+	String id;
+	String name;
+	String password;
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
