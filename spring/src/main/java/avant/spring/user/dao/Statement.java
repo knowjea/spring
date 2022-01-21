8a94a8070249935d41fac61db05e245cd11ec5cf
gyeongjaegwon <knowjea69@gmail.com>
2022-01-22 00:42:17 +0900

    내부 클래스

--- spring/src/main/java/avant/spring/user/dao/Statement.java
+++ spring/src/main/java/avant/spring/user/dao/Statement.java
@@ -49,12 +49,40 @@ public class Statement {
 	}
 
 	public void deleteAll() throws SQLException {
+		class DeleteAllStatement implements StatementStrategy {
+
+			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+				PreparedStatement ps;
+				ps = c.prepareStatement("delete from users");
+				return ps;
+			}
+
+		}
+
 		StatementStrategy st = new DeleteAllStatement();
 		jdbcContextWithStatementStrategy(st);
 	}
 
-	public void add(User user) throws SQLException {
-		StatementStrategy st = new AddStatement(user);
+	/**
+	 * 내부 클래스는 메서드내에서 정보를 공유하기 때문에 user를 따로 인스턴스 변수로 둘 필요가 없음.
+	 * 내부 클래스에서 외부의 변수를 사용할 때는 외부 변수는 반드시 final로 선언해줘야 한다. user 따라미터는 메소드 내부에서 변경될 일이 없으므로 final 로
+	 * 선언해도 무방하다.
+	 */
+	public void add(final User user) throws SQLException {
+		class AddStatement implements StatementStrategy {
+			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+
+				ps.setString(1, user.getId());
+				ps.setString(2, user.getName());
+				ps.setString(3, user.getPassword());
+
+				return ps;
+			}
+
+		}
+
+		StatementStrategy st = new AddStatement();
 		jdbcContextWithStatementStrategy(st);
 	}
 }
