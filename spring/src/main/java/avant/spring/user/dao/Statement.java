795b6a73061ec359c983ba8260732205efa6d185
gyeongjaegwon <knowjea69@gmail.com>
2022-01-22 00:15:36 +0900

    DI 적용한 전략패턴

---
+++ spring/src/main/java/avant/spring/user/dao/Statement.java
@@ -0,0 +1,53 @@
+package avant.spring.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+/**
+ * Hello world!
+ *
+ */
+public class Statement {
+	private DataSource dataSource;
+
+	public void setConnectionMaker(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+
+		try {
+			// 1. 커넥션 얻기
+			c = dataSource.getConnection();
+			// 2. 쿼리 수행
+			ps = stmt.makePreparedStatement(c);
+
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			// 3. 리소스 회수
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+			} catch (SQLException e) {}
+
+			try {
+				if (c != null) {
+					c.close();
+				}
+			} catch (SQLException e) {}
+		}
+	}
+
+	public void deleteAll() throws SQLException {
+		StatementStrategy st = new DeleteAllStatement();
+		jdbcContextWithStatementStrategy(st);
+	}
+}
